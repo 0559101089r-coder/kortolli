052c275499cda0e5055167f85a525783e0af6135
0559101089r-coder <0559101089r@gmail.com>
2025-12-03 19:31:27 +0600

    first commit

--- 37-1F-project/main.js
+++ 37-1F-project/main.js
@@ -1,15 +1,51 @@
 const loader = document.querySelector(".loader");
-const row = document.querySelector(".row");
+const row = document.querySelector(".row"); 
+
+
+const deleteItem = async (id) => {
+  if (!confirm(`Вы уверены, что хотите удалить товар с ID ${id}?`)) {
+    return;
+  }
+  
+  loader.style.display = "grid";
+  try {
+    const res = await fetch(`http://localhost:8000/products/${id}`, {
+      method: "DELETE", 
+    });
+    
+    if (res.ok) {
+        alert(`Товар успешно удален.`);
+  
+        await getData(); 
+    } else {
+        alert("Ошибка при удалении товара. Статус: " + res.status);
+    }
+  } catch (e) {
+    console.error("Ошибка при выполнении DELETE запроса:", e);
+    alert("Не удалось удалить товар из-за сетевой ошибки.");
+  } finally {
+    loader.style.display = "none";
+  }
+};
+
+
 
 const getData = async () => {
   loader.style.display = "grid";
   try {
     const res = await fetch("http://localhost:8000/products");
+    
+
+    if (!res.ok) {
+        throw new Error(`HTTP error! status: ${res.status}`);
+    }
+
     const data = await res.json();
 
     row.innerHTML = ""; 
 
     data.forEach((item) => {
+
       const oldPrice = Math.floor(item.price * 1.3);
 
       const card = document.createElement("div");
@@ -26,25 +62,27 @@ const getData = async () => {
              <p class="card-desc">${item.description}</p>
              
              <div class="price-block">
-                <span class="current-price">$${item.price}</span>
-                <span class="old-price">$${oldPrice}</span>
+               <span class="current-price">$${item.price}</span>
+               <span class="old-price">$${oldPrice}</span>
              </div>
 
              <div class="card-actions">
-                <a href="./pages/detail/detail.html?id=${item.id}" class="btn-primary">Подробнее</a>
-                <button class="btn-delete" onclick="deleteItem('${item.id}')">🗑</button>
+               <a href="./pages/detail/detail.html?id=${item.id}" class="btn-primary">Подробнее</a>
+               <button class="btn-delete" onclick="deleteItem('${item.id}')">🗑</button>
              </div>
           </div>
         </div>
       `;
       row.appendChild(card);
     });
+    
   } catch (error) {
-    console.log(error);
+    console.error("Ошибка при получении данных:", error);
   } finally {
     loader.style.display = "none";
   }
 };
 
 getData();
+
 
